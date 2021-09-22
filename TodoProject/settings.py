2618ab77440cb6eca12c7fb82b55e3c6b31ad138
AMRISH K.S <ksamrishsuresh@gmail.com>
2021-09-22 12:10:46 +0530

    todo project localhost project

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'TodoDB',
         'USER': 'postgres',
-        'PASSWORD': 'Sapspg@6',
+        'PASSWORD': 'pass',
         'HOST': 'localhost'
     }
 }
